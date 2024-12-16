c814ee467fc5fe68368bff61bdfd1c991c09ffed
Huan He <140455389+huanhe4096@users.noreply.github.com>
2024-12-15 22:57:16 -0500

    add panel for dashboard

--- web/src/views/DashboardView.vue
+++ web/src/views/DashboardView.vue
@@ -48,6 +48,65 @@ function onClickNewFile() {
 
 <div class="main flex-row">
 
+<Panel class="w-1/3 mr-2">
+    <template #header>
+        <div class="w-full flex justify-between">
+            <div class="flex">
+                <div class="flex-col">
+                    <div class="text-lg font-bold">
+                        <i class="fa-solid fa-briefcase"></i>
+                        Projects
+                    </div>
+                    <div class="panel-subtitle text-sm">
+                    </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </template>
+</Panel>
+
+<Panel class="w-1/3 mr-2">
+    <template #header>
+        <div class="w-full flex justify-between">
+            <div class="flex">
+                <div class="flex-col">
+                    <div class="text-lg font-bold">
+                        <i class="fa-solid fa-user"></i>
+                        Updates
+                    </div>
+                    <div class="panel-subtitle text-sm">
+                    </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </template>
+</Panel>
+
+<Panel class="w-1/3">
+    <template #header>
+        <div class="w-full flex justify-between">
+            <div class="flex">
+                <div class="flex-col">
+                    <div class="text-lg font-bold">
+                        <i class="fa-solid fa-user"></i>
+                        Statistics
+                    </div>
+                    <div class="panel-subtitle text-sm">
+                    </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </template>
+</Panel>
 
 </div>
 
